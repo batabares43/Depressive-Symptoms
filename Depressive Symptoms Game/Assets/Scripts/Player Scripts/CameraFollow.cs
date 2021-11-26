5805152c58d1aa68560c5047263d57639d37d695
batabares43 <batabares43@ucatolica.edu.co>
2021-11-26 09:06:01 -0500

    Teletransporte entre habitaciones

--- Depressive Symptoms Game/Assets/Scripts/Player Scripts/CameraFollow.cs
+++ Depressive Symptoms Game/Assets/Scripts/Player Scripts/CameraFollow.cs
@@ -5,23 +5,38 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField]private Transform player;
-    public float xmin;
-    public float xmax;
-    public float y;
-    
+    [SerializeField]private List<float> xmin = new List<float>();
+    [SerializeField]private List<float> xmax = new List<float>();
+    [SerializeField]private List<float> y = new List<float>();
+    [SerializeField] private List<Transform> targets = new List<Transform>();
 
+    [SerializeField]private int indexRoom=0;
+
+    public int IndexRoom { get=>indexRoom; set { indexRoom = value; changeCameraPosition(); } }
+    private void Start()
+    {
+        changeCameraPosition();
+    }
     private void Update()
     {
         followPlayer();
     }
     private void followPlayer(){
-        if (player.position.x > xmin && player.position.x < xmax)
+        float xPosition = player.position.x;
+        if (player.position.x < xmin[indexRoom])
         {
-            Vector3 newPosition = new Vector3(player.position.x, y, transform.position.z);
-            transform.position = newPosition;
+            xPosition = xmin[indexRoom];
         }
-
-        
-
+        else if (player.position.x > xmax[indexRoom])
+        {
+            xPosition = xmax[indexRoom];
+        }
+        Vector3 newPosition = new Vector3(xPosition, y[indexRoom], transform.position.z);
+        transform.position = newPosition;
+    }
+    private void changeCameraPosition()
+    {
+        Vector3 newPosition = new Vector3(targets[indexRoom].position.x, targets[indexRoom].position.y, transform.position.z);
+        transform.position = newPosition;
     }
 }
---
+++ Depressive Symptoms Game/Assets/Scripts/Travel Scripts/RoomTravel.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTravel : MonoBehaviour
+{
+    private Vector3 originalScale;
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform target;
+    [SerializeField] private int targetIndex;
+    [SerializeField] private float yOffset;
+    [SerializeField] private CameraFollow cameraTarget;
+    void Start()
+    {
+        originalScale = transform.localScale;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        cameraTarget = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraFollow>();
+    }
+    public void OnMouseOver()
+    {
+        if (!GameStateManager.Instance.InSelection)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                playerTravel();
+            }
+        }
+    }
+    private void playerTravel()
+    {
+        Vector3 newPosition = new Vector3(target.position.x, yOffset, player.position.z);
+        player.position = newPosition;
+        cameraTarget.IndexRoom = targetIndex;
+    }
+    private void OnMouseEnter()
+    {
+        if (!GameStateManager.Instance.InSelection)
+        {
+            float hoverEffect = 1.01f;
+            transform.localScale = new Vector3(transform.localScale.x * hoverEffect, transform.localScale.y * hoverEffect, transform.localScale.z);
+        }
+    }
+    private void OnMouseExit()
+    {
+        transform.localScale = originalScale;
+    }
+}
