5af8aa70ae1cf8b41f9d9f2759b3eb8082f76b5b
batabares43 <batabares43@ucatolica.edu.co>
2023-01-17 22:29:55 -0500

    Park and bar done

--- Depressive Symptoms Game/Assets/Scripts/Extra/DayNightCycle.cs
+++ Depressive Symptoms Game/Assets/Scripts/Extra/DayNightCycle.cs
@@ -6,27 +6,34 @@ using UnityEngine.UI;
 public class DayNightCycle : MonoBehaviour,Observer
 {
     [SerializeField] private Image panel;
-    [SerializeField] private Color c;
+    [SerializeField] private Color day;
+    [SerializeField] private Color evening;
+    [SerializeField] private Color night;
+
     public void updateState()
     {
-        panel = GameObject.Find("DayNight").GetComponent<Image>();
-        float minutes = (TimeManager.Instance.Minute * 100) / 60;
-        float temp = TimeManager.Instance.Hour;
-        temp = temp + 12;
-        temp = temp % 24;
-        float hour = temp+minutes/100;
+        int hour = TimeManager.Instance.Hour;
+        if ((hour >= 18 && hour<= 23) || (hour >= 0 && hour <= 6))
+        {
+            panel.color = night;
+        }else if(hour>=16 && hour <= 17)
+        {
+            panel.color = evening;
+        }
+        else
+        {
+            panel.color = day;
+        }
 
-        float result = Mathf.Sin((hour*Mathf.PI)/24);
-        temp = 100f / 255f;
-        result = result*temp;      
-        c.a = result;
-        panel.color = c;
+       
+
 
     }
 
    
     void Start()
     {
+        panel = GameObject.Find("DayNight").GetComponent<Image>();
         TimeManager.Instance.suscribe(this);
         updateState();
     }
